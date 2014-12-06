1d5291246a118ae1b330fdde761f279a70afc0eb
RomekRJM <rjm_20@o2.pl>
2014-12-06 23:47:37 +0000

    Lessons 27 - System Services (Cont.)

--- src/com/marakana/yamba/StatusActivity.java
+++ src/com/marakana/yamba/StatusActivity.java
@@ -4,19 +4,28 @@ import com.maracana.yamba.twitter.YambaApp;
 
 import android.app.Activity;
 import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class StatusActivity extends Activity implements OnClickListener {
+public class StatusActivity extends Activity implements OnClickListener, LocationListener {
 
 	private EditText editStatus;
 	private Button buttonUpdate;
 	private Context context;
+	private LocationManager locationManager;
+	private Location location;
+	
+	private static final String PROVIDER = LocationManager.GPS_PROVIDER;
+	private static final String TAG = "StatusActivity";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +40,39 @@ public class StatusActivity extends Activity implements OnClickListener {
 		context = this;
 
 		buttonUpdate.setOnClickListener(this);
+		
+		locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+		location = locationManager.getLastKnownLocation(PROVIDER);
+	}
+	
+	@Override
+	public void onLocationChanged(Location location) {
+		this.location = location;
+		Log.d(TAG, "On location changed: " + location.toString());
+	}
+	
+	@Override
+	public void onProviderDisabled(String arg0) {
+	}
+	
+	@Override
+	public void onProviderEnabled(String arg0) {
+	}
+	
+	@Override
+	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+	}	
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		locationManager.requestLocationUpdates(PROVIDER, 30000, 1000, this);
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		locationManager.removeUpdates(this);
 	}
 
 	@Override
@@ -68,6 +110,7 @@ public class StatusActivity extends Activity implements OnClickListener {
 			Toast.makeText(context, result, Toast.LENGTH_SHORT);
 		}
 
-	}	
+	}
+
 
 }
